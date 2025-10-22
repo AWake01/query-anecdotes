0159d8d508f9fd2aa2c019f49d9f2bf24a124319
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-10-22 15:06:05 +0100

    Query anecdote

---
+++ src/requests.jsx
@@ -0,0 +1,33 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+    const response = await fetch(baseUrl)
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch anecdotes')
+    }
+
+    return await response.json()
+}
+
+export const createAnecdote = async (newAnecdote) => {
+    console.log(newAnecdote)
+
+     if(newAnecdote.content.length < 5) {
+        throw new Error('Anecdote must be 5 characters or more')
+     }
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' } ,
+        body: JSON.stringify(newAnecdote)
+    }
+
+    const response = await fetch(baseUrl, options)
+
+    if(!response.ok) {
+        throw new Error('Failed to create anecdote')
+    }
+
+    return await response.json()
+}
